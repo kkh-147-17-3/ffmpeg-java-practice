2d1ffe016e84d9625703551243c883919310d055
Kyunghwan Kim <kkh147.17.3@gmail.com>
2023-02-18 22:50:42 +0900

    add: add configuratioin and properties

--- src/main/java/com/shoplive/web/backendtest/Config/AppConfig.java
+++ src/main/java/com/shoplive/web/backendtest/Config/AppConfig.java
@@ -1,15 +1,35 @@
-package com.shoplive.web.backendtest.Config;
+package com.shoplive.web.backendtest.config;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.shoplive.web.backendtest.Util.StorageProperties;
 
+import net.bramp.ffmpeg.FFmpeg;
+import net.bramp.ffmpeg.FFprobe;
+
 @Configuration
 public class AppConfig {
     
+    @Value("${ffmpeg.bin-path}")
+    private String ffmpegBinPath;
+
     @Bean
     public StorageProperties storageProperties() {
         return new StorageProperties(); 
+    }
+
+    @Bean 
+    FFmpeg ffMpeg() throws IOException{
+        return new FFmpeg(ffmpegBinPath+"ffmpeg");
+    }
+
+    @Bean
+    FFprobe ffProbe() throws IOException{
+        return new FFprobe(ffmpegBinPath+"ffprobe");
+
     }
 }

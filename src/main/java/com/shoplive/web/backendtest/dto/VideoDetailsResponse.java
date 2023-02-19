d2606b653a231cb18c47b1fea3977160d6950e25
kkh <kkh147.17.3@gmail.com>
2023-02-19 13:40:18 +0900

    add: add response & metainfo dto

---
+++ src/main/java/com/shoplive/web/backendtest/dto/VideoDetailsResponse.java
@@ -0,0 +1,24 @@
+package com.shoplive.web.backendtest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class VideoDetailsResponse {
+
+    private int id;
+    private String title;
+    private VideoMetaInfo original;
+    private VideoMetaInfo resized;
+    private String createdAt;
+    private String thumbnailUrl;
+}
---
+++ src/main/java/com/shoplive/web/backendtest/dto/VideoMetaInfo.java
@@ -0,0 +1,21 @@
+package com.shoplive.web.backendtest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class VideoMetaInfo {
+    private long filesize;
+    private int width;
+    private int height;
+    private String videoUrl;
+}

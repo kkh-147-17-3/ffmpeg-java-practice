290485761acfb05c08ab32bd09c6e45567cf9898
kkh <kkh147.17.3@gmail.com>
2023-02-20 16:50:50 +0900

    refactor: change return type of getProgress method

--- src/main/java/com/shoplive/web/backendtest/service/resize/VideoResizeService.java
+++ src/main/java/com/shoplive/web/backendtest/service/resize/VideoResizeService.java
@@ -1,6 +1,8 @@
 package com.shoplive.web.backendtest.service.resize;
 
+import com.shoplive.web.backendtest.dto.VideoProgressResponse;
+
 public interface VideoResizeService {
     public String create(Long videoId, String fileName);
-    public int getProgress(Long id);
+    public VideoProgressResponse getProgress(Long id);
 }

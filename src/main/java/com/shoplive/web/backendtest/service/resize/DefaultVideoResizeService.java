a10e50f1480f8b6a8b0f6b924d057457c04d0d5a
kkh <kkh147.17.3@gmail.com>
2023-02-20 16:52:24 +0900

    refactor: change return type of getProgress method

--- src/main/java/com/shoplive/web/backendtest/service/resize/DefaultVideoResizeService.java
+++ src/main/java/com/shoplive/web/backendtest/service/resize/DefaultVideoResizeService.java
@@ -2,6 +2,10 @@ package com.shoplive.web.backendtest.service.resize;
 
 import org.springframework.stereotype.Service;
 
+import com.shoplive.web.backendtest.dao.VideoDao;
+import com.shoplive.web.backendtest.dto.VideoProgressResponse;
+import com.shoplive.web.backendtest.entity.Video;
+import com.shoplive.web.backendtest.exception.VideoFileNotFoundException;
 import com.shoplive.web.backendtest.util.VideoUploadUtil;
 
 import lombok.RequiredArgsConstructor;
@@ -13,6 +17,8 @@ public class DefaultVideoResizeService implements VideoResizeService {
     private final VideoUploadUtil videoUploadUtil;
     private final int resizedWidth = 360;
 
+    private final VideoDao dao;
+
     @Override
     public String create(Long videoId, String fileName) {
 
@@ -21,9 +27,20 @@ public class DefaultVideoResizeService implements VideoResizeService {
     }
 
     @Override
-    public int getProgress(Long id) {
-        Integer progress =  videoUploadUtil.getProgressMap().get(id);
-        if (progress == null) return -1;
-        else return progress;
+    public VideoProgressResponse getProgress(Long videoId) {
+        Integer progress =  videoUploadUtil.getProgressMap().get(videoId);
+        // 작업 상태로 등록되지 않은 경우, 이미 완료되어 map에서 사라졌을 수 있으니 db에서 기등록여부를 확인한다.
+        if (progress == null) {
+            Video video = dao.getById(videoId);
+            if (video == null) throw new VideoFileNotFoundException("존재하지 않는 ID입니다. id: " + videoId);
+            else progress = 100;
+        }
+
+        String progressWithPercent = String.valueOf(progress) + "%";
+    
+        return  VideoProgressResponse.builder()
+                                    .id(videoId)
+                                    .progress(progressWithPercent)
+                                    .build();
     }
 }

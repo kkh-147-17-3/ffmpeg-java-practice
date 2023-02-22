677cc3521717389d6f65dc750fda33c2a2bf0735
kkh <kkh147.17.3@gmail.com>
2023-02-22 16:00:15 +0900

    feat: add getProgress method for checking thumbnailing progress

--- src/main/java/com/shoplive/web/backendtest/service/thumbnail/VideoThumbnailService.java
+++ src/main/java/com/shoplive/web/backendtest/service/thumbnail/VideoThumbnailService.java
@@ -2,8 +2,11 @@ package com.shoplive.web.backendtest.service.thumbnail;
 
 import java.io.IOException;
 
+import com.shoplive.web.backendtest.Response.VideoProgressResponse;
+
 public interface VideoThumbnailService {
 
     String createThumbnail(String fileName) throws IOException;
     
+    VideoProgressResponse getProgress(Long id);
 }

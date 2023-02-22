6220a66a52d21a1e44b4e26d481b12c65e7f101f
kkh <kkh147.17.3@gmail.com>
2023-02-22 15:57:12 +0900

    fix: rename the helper files

--- src/main/java/com/shoplive/web/backendtest/helper/FfmpegVideoResizer.java
+++ src/main/java/com/shoplive/web/backendtest/helper/FfmpegVideoResizer.java
@@ -17,11 +17,11 @@ import net.bramp.ffmpeg.progress.Progress.Status;
 import net.bramp.ffmpeg.progress.ProgressListener;
 
 @Component
-public class FfmpegVideoResizeHandler extends FfmpegVideoHandler implements ProgressibleVideoResizer{
+public class FfmpegVideoResizer extends FfmpegVideoHandler implements ProgressibleVideoResizer{
 
     ProgressHandler progressHandler;
 
-    public FfmpegVideoResizeHandler(FFmpeg ffmpeg, FFprobe ffprobe){
+    public FfmpegVideoResizer(FFmpeg ffmpeg, FFprobe ffprobe){
         super(ffmpeg, ffprobe);
         progressHandler = new ProgressHandler();
     }
@@ -31,21 +31,30 @@ public class FfmpegVideoResizeHandler extends FfmpegVideoHandler implements Prog
         FFmpegProbeResult probeResult = getProbeResult(targetFilePath);
         
         float aspectRatio = (float)probeResult.getStreams().get(0).width / probeResult.getStreams().get(0).height;
+        
         int resizeHeight = Math.round(resizeWidth / aspectRatio);
+        
+        // resize 시 height가 홀수인 경우 작업이 진행되지 않으므로 홀수 height에는 1을 더해 짝수로 맞춰준다.
+        // https://stackoverflow.com/questions/20847674/ffmpeg-libx264-height-not-divisible-by-2
         resizeHeight = (resizeHeight % 2 != 0) ? resizeHeight + 1 : resizeHeight;
 
+        //https://superuser.com/questions/714804/converting-video-from-1080p-to-720p-with-smallest-quality-loss-using-ffmpeg
         FFmpegBuilder builder = new FFmpegBuilder()
                                     .setInput(probeResult)
                                     .overrideOutputFiles(true)
                                     .addOutput(resultFilePath)
-                                    .setVideoResolution(resizeWidth, resizeHeight)
+                                    // aspect ratio를 유지하면서 width 기준으로 resizing
+                                    .setVideoFilter(String.format("scale=%d:-2", resizeWidth))
                                     .setStrict(FFmpegBuilder.Strict.EXPERIMENTAL)
+                                    // crf: 0에 가까울 수록 손실이 적으나, 통상적으로 18 이하에서는 시각적으로 확연한 차이가 없다고 함
+                                    .setConstantRateFactor(18)
+                                    // preset: ultrafast ~ veryslow 까지 선택할 수 있고, 빠를 수록 동일 품질 대비 용량이 크다.
+                                    .setPreset("slower")
                                     .done();
 
         FFmpegExecutor executable = new FFmpegExecutor(this.getFfMpeg(), this.getFfProbe());
         FFmpegJob job = executable.createJob(builder, new ProgressListener() {
 
-            // Using the FFmpegProbeResult determine the duration of the input
             final double durationNanosecs = probeResult.getFormat().duration * TimeUnit.SECONDS.toNanos(1);
         
             @Override
@@ -61,18 +70,7 @@ public class FfmpegVideoResizeHandler extends FfmpegVideoHandler implements Prog
                 } 
 
                 progressHandler.updateProgress(targetFilePath, percentage);
-                
 
-                // Print out interesting information about the progress
-                
-                System.out.println(String.format(
-                    "[%d] status:%s frame:%d fps:%.0f speed:%.2fx",
-                    percentage,
-                    progress.status,
-                    progress.frame,
-                    progress.fps.doubleValue(),
-                    progress.speed
-                ));
             }
         });
 
---
+++ src/main/java/com/shoplive/web/backendtest/helper/FfmpegVideoThumbnailer.java
@@ -0,0 +1,98 @@
+package com.shoplive.web.backendtest.helper;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.stereotype.Component;
+
+import com.shoplive.web.backendtest.interfaces.ProgressibleVideoThumbnailer;
+
+import net.bramp.ffmpeg.FFmpeg;
+import net.bramp.ffmpeg.FFmpegExecutor;
+import net.bramp.ffmpeg.FFprobe;
+import net.bramp.ffmpeg.builder.FFmpegBuilder;
+import net.bramp.ffmpeg.job.FFmpegJob;
+import net.bramp.ffmpeg.probe.FFmpegProbeResult;
+import net.bramp.ffmpeg.progress.Progress;
+import net.bramp.ffmpeg.progress.Progress.Status;
+import net.bramp.ffmpeg.progress.ProgressListener;
+
+@Component
+public class FfmpegVideoThumbnailer extends FfmpegVideoHandler implements ProgressibleVideoThumbnailer{
+
+    ProgressHandler progressHandler;
+
+    public FfmpegVideoThumbnailer(FFmpeg ffMpeg, FFprobe ffProbe) {
+        super(ffMpeg, ffProbe);
+        this.progressHandler = new ProgressHandler();
+    }
+
+    // TODO: 확장자별 차이 (.gif, .png, .jpg), 적정 thumbnail 크기, 비율 등...
+    @Override
+    public void createThumbnail(String targetFilePath, String resultFilePath) {
+        FFmpegProbeResult probeResult = getProbeResult(targetFilePath);
+
+        FFmpegBuilder builder = new FFmpegBuilder()	
+				.overrideOutputFiles(true)	
+                .setInput(targetFilePath)     				
+                .addExtraArgs("-ss", "00:00:01") 			
+                .addOutput(resultFilePath) 		
+                .setFrames(1)								
+                .done();    											
+
+		FFmpegExecutor executor = new FFmpegExecutor(this.getFfMpeg(), this.getFfProbe());		// FFmpeg 명령어 실행을 위한 FFmpegExecutor 객체 생성
+		FFmpegJob job = executor.createJob(builder, new ProgressListener() {
+
+            // Using the FFmpegProbeResult determine the duration of the input
+            final double durationNanosecs = probeResult.getFormat().duration * TimeUnit.SECONDS.toNanos(1);
+        
+            @Override
+            public void progress(Progress progress) {
+                
+                int percentage = 0;
+
+                if (progress.status == Status.END) {                        
+                    percentage = 100;
+                }
+                else{
+                    percentage = (int) (progress.out_time_ns / durationNanosecs * 100);
+                } 
+
+                progressHandler.updateProgress(targetFilePath, percentage);
+                
+
+                // Print out interesting information about the progress
+                
+                System.out.println(String.format(
+                    "[%d] thumbnail status:%s frame:%d fps:%.0f speed:%.2fx",
+                    percentage,
+                    progress.status,
+                    progress.frame,
+                    progress.fps.doubleValue(),
+                    progress.speed
+                ));
+            }
+        });
+        
+        
+        job.run();
+                
+    }
+
+    @Override
+    public Integer getProgress(String filePath){
+        return progressHandler.getProgress(filePath);
+    }
+
+    @Override
+    public void deleteProgress(String filePath) {
+        
+        progressHandler.deleteProgress(filePath);
+    }
+
+    @Override
+    public void updateProgress(String filePath, Integer percentage) {
+
+        progressHandler.updateProgress(filePath, percentage);
+    }
+    
+}
